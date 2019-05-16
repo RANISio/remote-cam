07cec4d44271668ecf31cc341dd84b2d5601943d
Ranis Eizen <ranis.eizen@gmail.com>
2019-05-16 18:54:36 -0400

    Dev progress

--- public/js/index.js
+++ public/js/index.js
@@ -1,70 +1,119 @@
-var peerConnection;
-var serverConnection = new WebSocket(`wss://${window.location.host}?room=${room}&role=${role}`);
+function app(room, role, isFirst) {
+  let stream;
+  let pc;
 
-var peerConnectionConfig = {
-  'iceServers': [
-    {'urls': 'stun:stun.stunprotocol.org:3478'},
-    {'urls': 'stun:stun.l.google.com:19302'},
-  ]
-};
+  const isSender = role === 's';
+  const player = document.querySelector('video');
+  const wss = new WebSocket(`wss://${window.location.host}?room=${room}&role=${role}`);
 
-serverConnection.onmessage = gotMessageFromServer;
+  wss.onmessage = gotMessageFromServer;
 
-function init(isSender, isCaller) {
-  console.log('isSender: ' + isSender)
-  console.log('isCaller: ' + isCaller)
-  peerConnection = new RTCPeerConnection(peerConnectionConfig);
-  peerConnection.onicecandidate = gotIceCandidate;
-  if(isSender) {
-    peerConnection.addStream(stream);
-  } else {
-    peerConnection.ontrack = gotRemoteStream;
+  wss.onopen = () => {
+    if(isSender) {
+      player.muted = true;
+      player.controls = true;
+      navigator.mediaDevices
+      .getUserMedia({
+        video: (() => {
+          if (navigator.userAgent.match(/Android/i)) {
+            return {
+              width: { ideal: 1920 },
+              height: { ideal: 1080 },
+              aspectRatio: 16 / 9,
+              facingMode: 'environment',
+              frameRate: { min: 25, max: 60 }
+            }
+          }
+          return {
+            facingMode: 'environment'
+          }
+        })(),
+        audio: true
+      })
+      .then(str => {
+        stream = str;
+        player.srcObject = str;
+        initPeerConnection();
+        if(!isFirst) { 
+          sendOffer();
+        }
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    } else {
+      initPeerConnection();
+      wss.send(JSON.stringify('connected'));
+    }
   }
-  if(isCaller) {
-    peerConnection.createOffer().then(createdDescription).catch(errorHandler);
-  }
-}
 
-function gotMessageFromServer(message) {
-  if(!peerConnection) init(role === 's',false);
-
-  var signal = JSON.parse(message.data);
-  console.log('Received messeage' + signal);
-  if(signal.sdp) {
-    peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function() {
-      // Only create answers in response to offers
-      if(signal.sdp.type == 'offer') {
-        peerConnection.createAnswer().then(createdDescription).catch(errorHandler);
+  function initPeerConnection() {
+    if(!pc) {
+      pc = new RTCPeerConnection({
+        'iceServers': [
+          {'urls': 'stun:stun.stunprotocol.org:3478'},
+          {'urls': 'stun:stun.l.google.com:19302'},
+        ]
+      });
+      pc.onicecandidate = gotIceCandidate;
+      if(isSender) {
+        pc.addStream(stream);
+      } else {
+        pc.ontrack = gotRemoteStream;
       }
+    }
+  }
+
+  function sendOffer() {
+    pc.createOffer().then(createdDescription).catch(errorHandler);
+  }
+
+  function gotMessageFromServer(message) {
+    initPeerConnection();
+    var signal = JSON.parse(message.data);
+    if(signal === 'connected') {
+      sendOffer();
+    } else if(signal.sdp) {
+      pc.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function() {
+        // Only create answers in response to offers
+        if(signal.sdp.type == 'offer') {
+          pc.createAnswer().then(createdDescription).catch(errorHandler);
+        }
+      }).catch(errorHandler);
+    } else if(signal.ice) {
+      pc.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
+    } else if(signal === 'disconnect') {
+      resetPc();
+    }
+  }
+
+  function gotIceCandidate(event) {
+    if(event.candidate != null) {
+      wss.send(JSON.stringify({'ice': event.candidate}));
+    }
+  }
+
+  function createdDescription(description) {
+    pc.setLocalDescription(description).then(function() {
+      wss.send(JSON.stringify({'sdp': pc.localDescription}));
     }).catch(errorHandler);
-  } else if(signal.ice) {
-    peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
-  } else if(signal === 'disconnect') {
-    peerConnection.close();
-    peerConnection = undefined;
   }
-}
 
-function gotIceCandidate(event) {
-  console.log('IceCandidate: ' + event.candidate);
-  if(event.candidate != null) {
-    serverConnection.send(JSON.stringify({'ice': event.candidate}));
+  function gotRemoteStream(event) {
+    player.srcObject = event.streams[0];
   }
-}
 
-function createdDescription(description) {
-  peerConnection.setLocalDescription(description).then(function() {
-    serverConnection.send(JSON.stringify({'sdp': peerConnection.localDescription}));
-  }).catch(errorHandler);
-}
+  function errorHandler(error) {
+    console.log(error);
+  }
 
-function gotRemoteStream(event) {
-  console.log('gotRemoteStream: ', event.streams)
-  player.srcObject = event.streams[0];
-}
+  function resetPc() {
+    pc.close();
+    pc = undefined;
+  }
 
-function errorHandler(error) {
-  console.log(error);
+  window.onbeforeunload = () => { 
+    wss.send(JSON.stringify('disconnect')); 
+    resetPc(); 
+  };
 }
-
-window.onbeforeunload = () => serverConnection.send(JSON.stringify('disconnect'));
